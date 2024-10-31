b73c6b0b2b797d45e628df465db04559e79cead1
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-10-31 18:24:58 +0400

    Add files via upload

--- static/js/Telegram.js
+++ static/js/Telegram.js
@@ -3,4 +3,4 @@
 var WebApp = window.Telegram.WebApp;
 const TelegramUserID = WebApp.WebAppUser.id; // user_id from inline mode
 
-document.getElementById('telegram_id').innerHTML = TelegramUserID;
+document.getElementById('telegram_id').innerHTML = 'ID: ', TelegramUserID;
